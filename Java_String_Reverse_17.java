79dacfb9eee44668e8783891ce79e07859f15985
NsPranto <nspranto6@gmail.com>
2023-05-30 14:00:47 +0600

    replaced random string with meaningful variables

--- Java_String_Reverse_17.java
+++ Java_String_Reverse_17.java
@@ -4,9 +4,9 @@ public class Java_String_Reverse_17 {
     public static void main(String[] args) {
         //https://www.hackerrank.com/challenges/java-string-reverse/problem?isFullScreen=true
         
-        Scanner sc=new Scanner(System.in);
-        String A=sc.next();
-        sc.close();
-        System.out.println( A.equals( new StringBuilder(A).reverse().toString()) ? "Yes" : "No" );      
+        Scanner input=new Scanner(System.in);
+        String reverse=input.next();
+        input.close();
+        System.out.println(reverse.equals(new StringBuilder(reverse).reverse().toString())?"Yes" : "No");      
     }
 }
