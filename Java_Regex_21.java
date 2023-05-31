67f2c471e41d7361f624af643db9e8fd1b4fab5a
NsPranto <nspranto6@gmail.com>
2023-05-31 13:28:51 +0600

    Url's added and resolve code running issues

--- Java_Regex_21.java
+++ Java_Regex_21.java
@@ -1,10 +1,13 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.Scanner;
 
 class Solution{
 
     public static void main(String[] args){
+        //https://www.hackerrank.com/challenges/java-regex/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+        //https://www.youtube.com/watch?v=e8LvIbWOdRM
         Scanner in = new Scanner(System.in);
-        in.close();
         while(in.hasNext()){
             String IP = in.next();
             System.out.println(IP.matches(new MyRegex().pattern));
@@ -12,9 +15,12 @@ class Solution{
 
     }
 }
-
-//Write your code here
+/*(Java regular expressions for Ip address)
+     [01]?\\d{1,2}   matches numbers 0-199.
+     2[0-4]\\d       matches numbers 200-249
+     25[0-5]         matches numbers 250-255
+*/
 class MyRegex {
-    String number = "([01]?\\d{1,2}|2[0-4|\\d|25[0-5])";
-    String pattern = number+"."+number+"."+number+"."+number;
+    String num = "([01]?\\d{1,2}|2[0-4]\\d|25[0-5])";
+    String pattern = num + "." +  num + "." +  num + "." + num;
 }
